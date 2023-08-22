cfc19d71d135e105deb7bf154fdd4570cc645e97
Felipe Silva <flpslvadev@gmail.com>
2023-08-22 10:55:25 -0300

    feat: jwt creating

---
+++ src/IdentityApi/Extensions/JwtSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace IdentityApi.Extensions;
+
+public class JwtSettings
+{
+    public string Secret { get; set; }
+    public string TimesExpires { get; set; }
+    public string Issuer { get; set; }
+    public string ValidIn { get; set; } 
+}
