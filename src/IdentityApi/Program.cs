13fd7f6e86222d64c2c4c1532c8a29421d3dd570
Felipe Silva <flpslvadev@gmail.com>
2023-08-22 10:28:45 -0300

    docs: config swagger

--- src/IdentityApi/Program.cs
+++ src/IdentityApi/Program.cs
@@ -1,6 +1,7 @@
 using IdentityApi.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,14 +15,26 @@ builder.Services.AddDefaultIdentity<IdentityUser>()
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Authorization API",
+        Description = "Discover the Project Authorization Authentication API.",
+        Contact = new OpenApiContact() { Name = "FlpSlva", Email = "flpslvadev@gmail.com" },
+        License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+    });
+});
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
