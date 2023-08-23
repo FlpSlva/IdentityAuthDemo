3c096feb6e77f2e93f364ea3deace76c0b120f3a
Felipe Silva <flpslvadev@gmail.com>
2023-08-23 13:23:37 -0300

    refactor: correcting property

--- src/IdentityApi/Extensions/JwtSettings.cs
+++ src/IdentityApi/Extensions/JwtSettings.cs
@@ -3,7 +3,7 @@
 public class JwtSettings
 {
     public string Secret { get; set; }
-    public string TimesExpires { get; set; }
+    public int TimesExpires { get; set; }
     public string Issuer { get; set; }
     public string ValidIn { get; set; } 
 }
