d2b338469bc9990fbe920de5c287ced2b4394c01
Felipe Silva <flpslvadev@gmail.com>
2023-08-24 10:40:04 -0300

    feat: init

---
+++ src/CustomerApi/Data/Mappings/CustomerMapping.cs
@@ -0,0 +1,16 @@
+﻿using CustomerApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CustomerApi.Data.Mappings;
+
+public class CustomerMapping : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.HasKey(_ => _.Id);
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasColumnType("varchar(100)");
+    }
+}
---
+++ src/CustomerApi/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿namespace CustomerApi.Models;
+
+public class Customer : Entity
+{
+    public Customer(string name, string email)
+        : base ()
+    {
+        Name = name;
+        Email = email;
+    }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+}
