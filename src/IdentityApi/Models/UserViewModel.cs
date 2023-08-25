035c430fd4f50d45b20069ffd1149e3b793e9ad1
Felipe Silva <flpslvadev@gmail.com>
2023-08-25 14:55:07 -0300

    feat: creating new class

--- src/IdentityApi/Models/UserViewModel.cs
+++ src/IdentityApi/Models/UserViewModel.cs
@@ -9,11 +9,15 @@ public class UserRegister
     public string Email { get; set; }
 
     [Required(ErrorMessage = "Field {0} is requerid")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "Field {0} is requerid")]
     [StringLength(100, ErrorMessage = "Field {0} must be between {2} and {1} characters", MinimumLength = 6)]
     public string Password { get; set; }
 
     [Compare("Password", ErrorMessage = "Passwords don't match.")]
     public string ConfirmPassword { get; set; }
+
 }
 
 public class UserLogin
@@ -45,4 +49,13 @@ public class UserClaim
 {
     public string Value { get; set; }
     public string Type { get; set; }
+}
+
+public class CustomerResponse
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+
 }
