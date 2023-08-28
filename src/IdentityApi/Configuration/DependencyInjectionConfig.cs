8595fd199e81cac0ca288f5eb0bca567dbe944f8
Felipe Silva <flpslvadev@gmail.com>
2023-08-28 10:14:26 -0300

    feat: dependency injection config

---
+++ src/IdentityApi/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,12 @@
+﻿using IdentityApi.services;
+
+namespace IdentityApi.Configuration;
+
+public static class DependencyInjectionConfig
+{
+    public static void ResolveDependence(this IServiceCollection services)
+    {
+
+        services.AddHttpClient<IAuthenticateService, AuthenticateService>();
+    }
+}
