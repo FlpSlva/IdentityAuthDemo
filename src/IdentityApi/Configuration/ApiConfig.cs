b0b6a06509f09665032b26315aa140702a2e86b9
Felipe Silva <flpslvadev@gmail.com>
2023-08-28 10:14:49 -0300

    feat: api config

---
+++ src/IdentityApi/Configuration/ApiConfig.cs
@@ -0,0 +1,30 @@
+﻿namespace IdentityApi.Configuration;
+
+public static class ApiConfig
+{
+    public static IServiceCollection AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        services.AddIdentityConfiguration(configuration);
+
+        services.ResolveDependence();
+
+        services.AddControllers();
+
+        services.AddSwaggerConfiguration();
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwaggerConfiguration();
+        }
+
+        app.UseAuthConfiguration();
+
+        return app;
+    }
+}
